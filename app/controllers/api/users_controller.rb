5c21d44dfd7876dea9ca630fef01a7daaefa02e7
Liz <lizelotte.green@gmail.com>
2015-10-06 18:23:37 -0500

    Added permission to list table, updated serializer association, added validations to list and item model, added create methods to list and item controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,35 +1,25 @@
 class Api::UsersController < ApiController
   before_action :authenticated?
 
-  respond_to :json
-
   def index
     users = User.all 
     
     render json: users, each_serializer: UserSerializer
   end
 
+  def create
+    user = User.new(user_params)
+      if user.save
+        render json: user
+      else
+        render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+      end
+  end
+
   private
 
-  # This is for the following checkpoint
-  
-  # def new 
-  #   user = User.new
-  # end
-
-  # def create
-  #     user = User.create(user_params)
-  #   if user.save
-  #     render json: user
-  #   else
-  #     render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
-  #   end
-  # end
-
-  # private
-
-  # def user_params
-  # params.require(:user).permit(:username, :password)
-  # end
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
 
 end
